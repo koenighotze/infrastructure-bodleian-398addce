a39f4a06ef07cf76a5b65ae1c80a3a21a1e4a8ac
David Schmitz <davidschmitz@posteo.de>
2022-04-14 09:28:48 +0200

    Avoid creating the cloud run backend for now

--- backend-cloud-run.tf
+++ backend-cloud-run.tf
@@ -1,43 +1,43 @@
-resource "google_cloud_run_service" "backend" {
-  name     = "backend"
-  location = var.region
+# resource "google_cloud_run_service" "backend" {
+#   name     = "backend"
+#   location = var.region
 
-  template {
-    spec {
-      containers {
-        image = "gcr.io/google-samples/hello-app:1.0"
-        env {
-          name  = "FOO"
-          value = "BAR"
-        }
-        ports {
-          container_port = 8080
-        }
-      }
+#   template {
+#     spec {
+#       containers {
+#         image = "gcr.io/google-samples/hello-app:1.0"
+#         env {
+#           name  = "FOO"
+#           value = "BAR"
+#         }
+#         ports {
+#           container_port = 8080
+#         }
+#       }
 
-      container_concurrency = 3
-      timeout_seconds       = 3
-      service_account_name  = data.google_service_account.backend_runtime_sa.email
-    }
-  }
+#       container_concurrency = 3
+#       timeout_seconds       = 3
+#       service_account_name  = data.google_service_account.backend_runtime_sa.email
+#     }
+#   }
 
-  metadata {
-    labels = {
-      "application" = "bodleian"
-      "stage"       = "dev"
-    }
-  }
+#   metadata {
+#     labels = {
+#       "application" = "bodleian"
+#       "stage"       = "dev"
+#     }
+#   }
 
-  lifecycle {
-    ignore_changes = [
-      metadata.0.annotations,
-    ]
-  }
+#   lifecycle {
+#     ignore_changes = [
+#       metadata.0.annotations,
+#     ]
+#   }
 
-  traffic {
-    percent         = 100
-    latest_revision = true
-  }
-}
+#   traffic {
+#     percent         = 100
+#     latest_revision = true
+#   }
+# }
 
 
