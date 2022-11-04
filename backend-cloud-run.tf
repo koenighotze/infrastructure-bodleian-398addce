61bc8c2fec7ad8614bb9eeb275fbc92465b5be8a
David Schmitz <davidschmitz@posteo.de>
2022-11-04 22:56:15 +0100

    Disable annotation for the time being

--- backend-cloud-run.tf
+++ backend-cloud-run.tf
@@ -27,9 +27,9 @@ resource "google_cloud_run_service" "backend" {
       "application" = "bodleian"
       "stage"       = "dev"
     }
-    annotations = {
-      "autoscaling.knative.dev/maxScale" = "3"
-    }
+    # annotations = {
+    #   "autoscaling.knative.dev/maxScale" = "3"
+    # }
   }
 
   lifecycle {
